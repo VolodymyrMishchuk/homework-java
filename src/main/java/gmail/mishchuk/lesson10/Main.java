f22ca3134a342717ffad33931f167a232e61bd50
VolodymyrMishchuk <pidsercem@gmail.com>
2025-03-03 16:39:10 +0100

    test3

--- src/main/java/gmail/mishchuk/lesson10/Main.java
+++ src/main/java/gmail/mishchuk/lesson10/Main.java
@@ -5,6 +5,17 @@ public class Main {
 
         int [] array = new int[20];
 
-        for (int i = 0; i !"§$%&/()°")
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random() * 100);
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
     }
 }
