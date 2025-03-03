90d4f46da55617710f10d9a2e3775571c8a306a3
VolodymyrMishchuk <pidsercem@gmail.com>
2025-03-03 16:36:50 +0100

    test2

---
+++ src/main/java/gmail/mishchuk/lesson10/Main.java
@@ -0,0 +1,10 @@
+package gmail.mishchuk.lesson10;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] array = new int[20];
+
+        for (int i = 0; i !"§$%&/()°")
+    }
+}
