b425b298141282e606c8f050c17071398b2d95be
VolodymyrMishchuk <pidsercem@gmail.com>
2025-03-04 17:03:30 +0100

    test3

--- src/main/java/gmail/mishchuk/lesson11/Main.java
+++ src/main/java/gmail/mishchuk/lesson11/Main.java
@@ -1,4 +1,21 @@
 package gmail.mishchuk.lesson11;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int numDayOfWeek = (int)(Math.random() * 8);
+
+        var result = switch(numDayOfWeek) {
+            case 1 -> "Понеділок";
+            case 2 -> "Вівторок";
+            case 3 -> "Середа";
+            case 4 -> "Четвер";
+            case 5 -> "П'ятниця";
+            case 6 -> "Субота";
+            case 7 -> "Неділя";
+            default -> "хз, як так вийшло";
+        };
+
+        System.out.println(result);
+    }
 }
