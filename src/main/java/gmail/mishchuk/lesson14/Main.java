14988ba6e1c99d3222ff2ea82f9337cac4c44c77
VolodymyrMishchuk <pidsercem@gmail.com>
2025-03-08 22:54:48 +0100

    extra

--- src/main/java/gmail/mishchuk/lesson14/Main.java
+++ src/main/java/gmail/mishchuk/lesson14/Main.java
@@ -1,4 +1,20 @@
 package gmail.mishchuk.lesson14;
 
+import gmail.mishchuk.lesson14.quasiAnimals.Dragon;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Dragon dragon = new Dragon();
+        dragon.setName("Drago_1");
+        dragon.setAge(20);
+        dragon.setWeight(108.9);
+
+        System.out.println(dragon.getName());
+        System.out.println(dragon.getAge());
+        System.out.println(dragon.getWeight());
+
+        Dragon dragon2 = new Dragon("Drago_2", 15, 97.5);
+        System.out.println(dragon2.toString());
+    }
 }
--- src/main/java/gmail/mishchuk/lesson14/quasiAnimals/Dragon.java
+++ src/main/java/gmail/mishchuk/lesson14/quasiAnimals/Dragon.java
@@ -1,7 +1,54 @@
-package gmail.mishchuk.lesson14;
+package gmail.mishchuk.lesson14.quasiAnimals;
+
+import java.sql.SQLOutput;
 
 public class Dragon {
+
     private String name;
     private int age;
     private double weight;
+
+    public Dragon() {
+        System.out.println("Створено невідомого дракона");
+    }
+
+    public Dragon(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        System.out.println("Створено повноцінного дракона");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Dragon{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
 }
