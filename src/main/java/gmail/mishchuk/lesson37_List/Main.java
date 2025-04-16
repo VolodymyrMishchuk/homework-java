a9a3a803a2e5670fe80c3bcf532f854eaa15ed95
volodymyr.mishchuk <pidsercem@gmail.com>
2025-04-16 16:09:58 +0200

    homework #37 update

--- src/main/java/gmail/mishchuk/lesson37_List/Main.java
+++ src/main/java/gmail/mishchuk/lesson37_List/Main.java
@@ -1,20 +1,37 @@
 package gmail.mishchuk.lesson37_List;
 
-import java.util.List;
+import java.util.*;
 
 import static gmail.mishchuk.lesson37_List.NumberUtils.*;
+import static gmail.mishchuk.lesson37_List.WordUtils.*;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Number> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int sumGeneral = getSum(numbers);
-        List<Integer> resultMultiplyOddNumber = multiplyOddNumber(numbers);
+        getSum(numbers);
+        getMultiplyOddNumber(numbers);
 
-        System.out.println("Якщо взяти і додадати прям всі цифри, то вийде " + sumGeneral
+        List<String> words = Arrays.asList("we", "are", "looking", "for", "for", "different", "words");
+
+        getShortestWord(words);
+        getTheUniqWords(words);
+        getDuplicateWords(words);
+        Map<String, Integer> entries = getEntriesOfWords(words);
+
+        System.out.println("\nЯкщо взяти і додадати прям всі цифри з numbers, то вийде " + getSum(numbers)
                 + "\n\nА якщо виконати ще трішки дивної логіки"
-                + "\n(ну хз для чого, але таке завдання було)"
-                + "\nто отримаємо: " + resultMultiplyOddNumber);
+                + "\n(ну хз для чого, але таке завдання було),"
+                + "\nто отримаємо: " + getMultiplyOddNumber(numbers));
+
+        System.out.println("\nА якщо опрацювати words, то ми зможемо"
+                + "\nЗнайти найкоротше слово: [" + getShortestWord(words) + "]"
+                + "\nВідібрати унікальні слова: " + getTheUniqWords(words)
+                + "\nВизначити які слова дублюються: " + getDuplicateWords(words)
+                + "\nНавіть дізнаємося скільки разів яке слово вживається, доприкладу: ");
+        for (Map.Entry<String, Integer> entry : entries.entrySet()) {
+            System.out.println("[" + entry.getKey() + " --> " + entry.getValue() + "]");
+        }
     }
 }
