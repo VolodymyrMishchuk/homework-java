d6395d45df51378accd1632c3ac0e7351f631335
volodymyr.mishchuk <pidsercem@gmail.com>
2025-04-16 16:24:53 +0200

    homework #37 up-update

--- src/main/java/gmail/mishchuk/lesson37_List/Main.java
+++ src/main/java/gmail/mishchuk/lesson37_List/Main.java
@@ -13,6 +13,11 @@ public class Main {
         getSum(numbers);
         getMultiplyOddNumber(numbers);
 
+        System.out.println("\nЯкщо взяти і додадати прям всі цифри з numbers, то вийде " + getSum(numbers)
+                + "\n\nА якщо виконати ще трішки дивної логіки"
+                + "\n(ну хз для чого, але таке завдання було),"
+                + "\nто отримаємо: " + getMultiplyOddNumber(numbers));
+
         List<String> words = Arrays.asList("we", "are", "looking", "for", "for", "different", "words");
 
         getShortestWord(words);
@@ -20,18 +25,13 @@ public class Main {
         getDuplicateWords(words);
         Map<String, Integer> entries = getEntriesOfWords(words);
 
-        System.out.println("\nЯкщо взяти і додадати прям всі цифри з numbers, то вийде " + getSum(numbers)
-                + "\n\nА якщо виконати ще трішки дивної логіки"
-                + "\n(ну хз для чого, але таке завдання було),"
-                + "\nто отримаємо: " + getMultiplyOddNumber(numbers));
-
         System.out.println("\nА якщо опрацювати words, то ми зможемо"
                 + "\nЗнайти найкоротше слово: [" + getShortestWord(words) + "]"
                 + "\nВідібрати унікальні слова: " + getTheUniqWords(words)
                 + "\nВизначити які слова дублюються: " + getDuplicateWords(words)
                 + "\nНавіть дізнаємося скільки разів яке слово вживається, доприкладу: ");
         for (Map.Entry<String, Integer> entry : entries.entrySet()) {
-            System.out.println("[" + entry.getKey() + " --> " + entry.getValue() + "]");
+            System.out.println("\t[" + entry.getKey() + " --> " + entry.getValue() + "]");
         }
     }
 }
