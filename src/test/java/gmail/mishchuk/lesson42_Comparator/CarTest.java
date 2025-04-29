93de524cb50c7525714bcaf3543d923d09ee8e4a
volodymyr.mishchuk <pidsercem@gmail.com>
2025-04-29 09:03:10 +0200

    rename values on the test

--- src/test/java/gmail/mishchuk/lesson42_Comparator/CarTest.java
+++ src/test/java/gmail/mishchuk/lesson42_Comparator/CarTest.java
@@ -15,21 +15,21 @@ class CarTest {
         treeSetOfCar.add(new Car(2018, 4.3));
 
         // when
-        Set<Car> sortedListOfCar = new TreeSet<>(Comparator.comparing(Car::getYearOfManufacture));
-        sortedListOfCar.addAll(treeSetOfCar);
+        Set<Car> sortedTreeSetOfCar = new TreeSet<>(Comparator.comparing(Car::getYearOfManufacture));
+        sortedTreeSetOfCar.addAll(treeSetOfCar);
 
         // then
-        Set<Car> testSetOfCar = new TreeSet<>();
-        testSetOfCar.add(new Car(2012, 2.0));
-        testSetOfCar.add(new Car(2018, 4.3));
-        testSetOfCar.add(new Car(2023, 2.3));
+        Set<Car> testTreeSetOfCar = new TreeSet<>();
+        testTreeSetOfCar.add(new Car(2012, 2.0));
+        testTreeSetOfCar.add(new Car(2018, 4.3));
+        testTreeSetOfCar.add(new Car(2023, 2.3));
 
         // чому тут не має помилки?
         // що не так зробив?
-        // адже: (treeSetOfCar, testSetOfCar) завідомо є різними
-        assertEquals(treeSetOfCar, testSetOfCar);
-        assertEquals(sortedListOfCar, testSetOfCar);
+        // адже: (treeSetOfCar, testTreeSetOfCar) завідомо є різними
+        assertEquals(treeSetOfCar, testTreeSetOfCar);
+        assertEquals(sortedTreeSetOfCar, testTreeSetOfCar);
 
-        assertEquals(sortedListOfCar.size(), testSetOfCar.size());
+        assertEquals(sortedTreeSetOfCar.size(), testTreeSetOfCar.size());
     }
 }
